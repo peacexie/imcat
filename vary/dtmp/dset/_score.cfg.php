f01655a5a2670d9f72cc2265c19e20f37d29d363
peacexie <test>
2016-09-14 14:26:57 +0800

    cfg_db.php

--- vary/dtmp/dset/_score.cfg.php
+++ vary/dtmp/dset/_score.cfg.php
@@ -5,7 +5,7 @@ $_cbase['sys_open'] = "4";
 $_cbase['sys_pop'] = "3";
 $_cbase['msg_timea'] = "1500";
 $_cbase['sys_map'] = "baidu";
-$_cbase['sys_name'] = "CanPHP 2.X for Peace";
+$_cbase['sys_name'] = "贴心猫(TiexinCat)";
 
 $_cbase['adm_mode'] = "expert";
 $_cbase['debug']['err_mode'] = "1";
@@ -26,8 +26,8 @@ $_cbase['safe']['rnum'] = "894539991563189632443894";
 $_cbase['safe']['rspe'] = "spe-T_)NTXN5-@I4+28D5-cRqEwdyD";
 $_cbase['safe']['rndch6'] = "hL4.bF";
 $_cbase['safe']['rndtab'] = "7CFtrx4Lubi0Kqh8IR5c3XyjplN1BGTHwDsVYezvao9O2APESJknZ6dfgmMQUW";
-$_cbase['tout_admin'] = "4";
-$_cbase['tout_member'] = "4";
+$_cbase['tout_admin'] = "72";
+$_cbase['tout_member'] = "96";
 
 $_cbase['show']['apsize'] = "20";
 $_cbase['show']['mpsize'] = "20";
